1c3d9caa8359be114f4ed55b7266c1fb4ccceb93
oyo <oyo@OYOadminChangeThisNameDumbo.local>
2020-09-05 07:11:08 +0530

    Updates

---
+++ precache-manifest.1e3b74df148416f7ba8393ac70baca40.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41afbbf3247308a10efd6a9857a53024",
+    "url": "/pomodore-clock/index.html"
+  },
+  {
+    "revision": "c9d002b6738e6162c206",
+    "url": "/pomodore-clock/static/css/main.5eb813dc.chunk.css"
+  },
+  {
+    "revision": "27e6b70194c85d45558f",
+    "url": "/pomodore-clock/static/js/2.aa457ddc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pomodore-clock/static/js/2.aa457ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d002b6738e6162c206",
+    "url": "/pomodore-clock/static/js/main.50f5a0c1.chunk.js"
+  },
+  {
+    "revision": "1118f5e2fc5b780d8873",
+    "url": "/pomodore-clock/static/js/runtime-main.238cfa6e.js"
+  },
+  {
+    "revision": "47f91d3c18c094447076b81f340ea15e",
+    "url": "/pomodore-clock/static/media/expand_less-24px.47f91d3c.svg"
+  },
+  {
+    "revision": "95a46151caf2550da82be2575a6b881c",
+    "url": "/pomodore-clock/static/media/expand_more-24px.95a46151.svg"
+  },
+  {
+    "revision": "0d842eebb9daead67d3f658138747a90",
+    "url": "/pomodore-clock/static/media/pause-24px.0d842eeb.svg"
+  },
+  {
+    "revision": "f5408bdfa39ea5734e938267bda43cbe",
+    "url": "/pomodore-clock/static/media/play_arrow-24px.f5408bdf.svg"
+  },
+  {
+    "revision": "0ebb9c8958bea398adc4150e97e614ce",
+    "url": "/pomodore-clock/static/media/restore-24px.0ebb9c89.svg"
+  }
+]);
